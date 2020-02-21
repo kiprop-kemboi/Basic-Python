d2505c65eb909296c42156c1869c8901d70e2e25
kiprop-kemboi <61315965+kiprop-kemboi@users.noreply.github.com>
2020-02-22 00:05:49 +0300

    Update controls.py
    demonstration

--- controls.py
+++ controls.py
@@ -1,14 +1,14 @@
-men = str(input("choose the name of your shoe brand\n\t1.hush puppies\n\t2.executive\n\t3.ambassador\n\t4.bata\n\t5.weinbrener"))
-women = str(input("choose your brand favourite shoe\n\t1.malona bloniks\n\t2.ballerina\n\t3.comfits\n\t4.marie claire\n\t5.red label"))
-if(men == 1):
+men = str(input("choose the name of your shoe brand\n\t1.hush puppies\n\t2.executive\n\t3.ambassador\n\t4.bata\n\t5.weinbrener\n"))
+if("your shoe brand is {hush puppies}"):
     print("price tag is ksh 6999")
-elif(men == 2):
+elif("Your shoe brand is {executive}"):
     print("price is ksh4999")
-elif(men == 3):
+elif("YOur shoe brand is {ambassador}"):
     print("price is 5999")
-elif(men == 4):
+elif("Your shoe brand is {bata}"):
     print("price is ksh3399")
-elif(men == 5):
+elif("Your shoe brand is{weinbrener}"):
     print("price is ksh4499")
 else:
     ("the input entered is invalid")
+
