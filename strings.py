f352989d17c3e9c0c121b5d037198474d13eda10
EarthmanAman <hashimathman@gmail.com>
2020-02-21 16:15:01 +0300

    input

--- strings.py
+++ strings.py
@@ -1,3 +1,23 @@
-print("hello world") #corrected
-a = "*\t*\t*\t*\n\t*\t*\n\t  *\n\t*\t*\n*\t*\t*\t*"
-print(a)
+a = 5
+
+"""
+
+String Reviews
+
+"""
+c = "The answer is {}".format(a) # Formatting a string
+d = f"The anser is {a}" # Another format
+
+
+"""
+
+User Inputs
+
+"""
+
+b = int(input("Enter the operation by selecting the number: \n\t1 . Addition \n\t2. Substration\n"))
+
+if(b == 1):
+	print(b)
+else:
+	print("Substration")
