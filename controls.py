948220cd98a82757b583f9e9e725f5cdca065f8e
kiprop-kemboi <61315965+kiprop-kemboi@users.noreply.github.com>
2020-02-21 23:39:55 +0300

    Create controls.py
    controls

---
+++ controls.py
@@ -0,0 +1,14 @@
+men = str(input("choose the name of your shoe brand\n\t1.hush puppies\n\t2.executive\n\t3.ambassador\n\t4.bata\n\t5.weinbrener"))
+women = str(input("choose your brand favourite shoe\n\t1.malona bloniks\n\t2.ballerina\n\t3.comfits\n\t4.marie claire\n\t5.red label"))
+if(men == 1):
+    print("price tag is ksh 6999")
+elif(men == 2):
+    print("price is ksh4999")
+elif(men == 3):
+    print("price is 5999")
+elif(men == 4):
+    print("price is ksh3399")
+elif(men == 5):
+    print("price is ksh4499")
+else:
+    ("the input entered is invalid")
