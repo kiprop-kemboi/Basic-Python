361615ed18bd8b9a267919cd006a3813ba13f0db
kiprop-kemboi <61315965+kiprop-kemboi@users.noreply.github.com>
2020-03-02 00:01:48 +0300

    Create collection.py
    trial

---
+++ collection.py
@@ -0,0 +1,34 @@
+#sets
+list = {1, 2, 3, 4}
+print(list)
+list.add(5)
+print(list)
+list.add(2)
+print(list)
+list.update([1, 2, 3])
+print(list)
+list.remove(5)
+print(list)
+
+
+#set()functions
+A = {1, 2, 3, 4}
+B = {3, 4, 5, 6, 7}
+print(A^B)
+print(B|A)
+print(A-B)
+print(A&B)
+
+
+
+#dictonary in python
+my_dict = {1:'alex', 2:'allen', 3:'bill'}
+print(my_dict[3])
+
+
+#range
+numbers = range(1, 6)
+print(set('numbers'))
+print(tuple('numbers'))
+
+
