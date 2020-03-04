db41b5ad4c6a61bfb4e8f9bd9db97a417e9e23c9
kiprop-kemboi <61315965+kiprop-kemboi@users.noreply.github.com>
2020-03-04 22:06:36 +0300

    Create iteration.py
    practical

---
+++ iteration.py
@@ -0,0 +1,38 @@
+n = 100
+sum = 0
+i = 1
+while (i <= n):
+    sum = sum + 1
+    i = i + 1
+print("the sum is", sum)
+print("the end")
+
+
+#for loop
+"""the line code below shall illustrate how for loops works on our code"""
+numbers = [6, 5, 3, 2, 4, 3]
+
+sum = 0
+for val in numbers:
+    sum = sum + val
+    print("the val of numbers", sum)
+
+    #break staments
+shoe =["bata", "executive", "tommy"]
+user = str(input("enter the type of shoe\n"))
+
+for n in shoe:
+    if shoe == "bata":
+
+        print("execellent")
+        break
+    elif shoe == "executive":
+
+        print("officially warrior")
+        break
+    elif shoe == "tommy":
+
+        print("casual maker of today")
+        break
+        print("the end")
+
